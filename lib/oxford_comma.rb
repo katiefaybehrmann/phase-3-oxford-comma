eb3aaeab6fefd57a6d1d542681041436abff8145
Katie <katiefaybehrmann@gmail.com>
2023-02-19 09:01:21 -0700

    commit tree 289084b806e31189b32a60d03cc748a8c5b44b07

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+    if array.length == 1
+        array.pop
+    elsif array.length == 2
+        sentence = array.join(" and ")
+    else
+        array[-1] = "and #{array[-1]}"
+        sentence = array.join(", ")
+    end
 end
